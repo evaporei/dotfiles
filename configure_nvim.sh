73c8d3acf51fe4a31f7cfb9f741096e694739567
Eva Pace <eba.pachi@gmail.com>
2023-07-17 19:32:18 -0300

    vim: use paru instead of yay in install script

--- configure_nvim.sh
+++ configure_nvim.sh
@@ -8,11 +8,8 @@ cp init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# check yay
-sudo yaourt -S yay
-
 # install pip
-yay -Sy python-pip
+paru -S python-pip
 
 sudo pip install neovim
 
