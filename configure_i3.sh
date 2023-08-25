687c0f874c502c23c1a3e9bd438f03139351b31b
Eva Pace <eba.pachi@gmail.com>
2023-08-25 11:20:50 -0300

    i3: add i3status base conf

--- configure_i3.sh
+++ configure_i3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
 
 cp i3config ~/.config/i3/config
+cp i3status.conf ~/.config/i3status/config
 
 cp evangelion_bg.jpg ~/Downloads
 cp evangelion_lock_screen.jpg ~/Downloads
