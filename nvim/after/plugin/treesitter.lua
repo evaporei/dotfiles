93dd61377a923a064974404d1057178783c921e5
Eva Pace <eba.pachi@gmail.com>
2023-10-01 10:34:14 -0300

    vim: disable treesitter highlighting (it's shit)

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -10,9 +10,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
-    enable = true,
-
-    disable = { "rust", "c" },
+    enable = false,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
