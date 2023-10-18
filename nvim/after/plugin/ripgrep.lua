f7c8144ed4bdd193e5089723fa2509601f18e92f
Eva Pace <eba.pachi@gmail.com>
2023-10-18 14:12:25 -0300

    vim: wip ripgrep

---
+++ nvim/after/plugin/ripgrep.lua
@@ -0,0 +1,12 @@
+-- nothing worksss
+-- vim.g.ackprg = 'rg --vimgrep --smart-case'
+-- vim.keymap.set('n', '<C-F>', ':Ack!<space>')
+--
+-- cnoreabbrev ag Ack
+-- cnoreabbrev aG Ack
+-- cnoreabbrev Ag Ack
+-- cnoreabbrev AG Ack
+-- vim.cmd.inoreabbrev({ "<buffer>", "ag", "Ack" })
+-- vim.cmd.inoreabbrev({ "<buffer>", "aG", "Ack" })
+-- vim.cmd.inoreabbrev({ "<buffer>", "Ag", "Ack" })
+-- vim.cmd.inoreabbrev({ "<buffer>", "AG", "Ack" })
