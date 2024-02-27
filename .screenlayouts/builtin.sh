d13d5e7734353d0640c9f1d29325c7c41658e587
Eva Pace <eba.pachi@gmail.com>
2024-02-27 13:32:33 -0300

    screenlayouts: add scripts for current thinkpad

---
+++ .screenlayouts/builtin.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP-1 -s 1920x1080 --auto --scale 1.25x1.25
+xrandr --output HDMI-1 --off
+xrandr --dpi 70
+feh --bg-scale ~/Downloads/triforce4k.jpg
---
+++ .screenlayouts/momonitor.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+xrandr --output DP-1 -s 3840x2160 --auto
+xrandr --output eDP-1 --off
+xrandr --dpi 222
+feh --bg-scale ~/Downloads/triforce4k.jpg
+
+# xrandr --output HDMI-1 -s 3840x2160 --output eDP-1 --off --dpi 200
+# xrandr --output HDMI-1 -s 3840x2160 --output eDP-1 --off --dpi 200
+# xrandr -s 2560x1440 --output HDMI-1 --output eDP-1 --off --dpi 200
+# xrandr -s 1920x1080 --output HDMI-1 --output eDP-1 --off --dpi 200
+# xrandr --dpi 200
